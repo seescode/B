5a76b6e5aaabc0d6468881922794f51ec622226d
seescode <seesyong@gmail.com>
2016-05-16 13:44:00 -0400

    Add grunt bump

---
+++ grunt/bump.js
@@ -0,0 +1,21 @@
+module.exports = {
+  bump: {
+    options: {
+      files: ['package.json'],
+      updateConfigs: [],
+      commit: true,
+      commitMessage: 'Release v%VERSION%',
+      commitFiles: ['package.json'],
+      createTag: true,
+      tagName: 'v%VERSION%',
+      tagMessage: 'Version %VERSION%',
+      push: true,
+      pushTo: 'upstream',
+      gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+      globalReplace: false,
+      prereleaseName: false,
+      metadata: '',
+      regExp: false
+    }
+  }
+};
